e1ab34c40d770a8b4c24e8c61cd67ea45461fc13
andychen482 <andy.chen0607@gmail.com>
2024-03-20 11:49:26 -0400

    Fixed file paths

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('.'));
+app.use(express.static('./'));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
